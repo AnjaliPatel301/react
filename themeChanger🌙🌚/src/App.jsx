0aea5211e6c062e1988a833683b4934ad353585c
Anjali <sj5065070@gmail.com>
2025-09-03 00:39:10 +0530

    Add files via upload

---
+++ themeChanger🌙🌚/src/App.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import ThemeBtn from './component/ThemeBtn'
+import Card from './component/card'
+import { ThemeProvider } from './context/Theme'
+
+
+function App() {
+  
+  const [themeMode,setthemeMode]=useState("light")
+  const lightMode=()=>{
+    setthemeMode("light")
+  } 
+  const darkMode=()=>{
+    setthemeMode("dark")
+  }
+  
+  useEffect(() => {
+    document.querySelector('html').classList.remove("light","dark")
+    document.querySelector('html').classList.add(themeMode)
+  
+  
+  }, [themeMode])
+  
+  return ( 
+<ThemeProvider value={{ themeMode,lightMode,darkMode}} >
+
+  
+<div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                       <ThemeBtn/>
+                    </div>
+
+                    <div className="w-full max-w-sm mx-auto">
+                       <Card/>
+                    </div>
+                </div>
+            </div>
+
+  </ThemeProvider>
+  )
+}
+
+export default App
