179f645ac23baf53fbd321a576ea94f3507f9ff0
Anjali <sj5065070@gmail.com>
2025-10-01 17:44:36 +0530

    Add files via upload

---
+++ useReducer/src/db/product.js
@@ -0,0 +1,284 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export const products=[
+   
+                {
+                    _id: uuidv4(),
+                    title: "Spring and summershoes",
+                    price: 20,
+                    img:'https://tse1.mm.bing.net/th/id/OIP._t5LKx5aq5VS9rdFnQde-wHaIy?pid=Api&P=0&h=180',
+                   quantity: 3,
+                    total: 60,
+                    discountPercentage: 8.71,
+                    discountedPrice: 55,
+                    thumbnail: "https://cdn.dummyjson.com/product-images/59/thumbnail.jpg"
+                },
+                {
+                    _id: uuidv4(),
+                    title: "TC Reusable Silicone Magic Washing Gloves",
+                    price: 29,
+                    img:'https://i5.walmartimages.com/asr/d4d05625-5285-48d9-90b2-68dfaefee397_1.e532b242ecfb39168539d19f9e77d981.jpeg',
+                   quantity: 2,
+                    total: 58,
+                    discountPercentage: 3.19,
+                    discountedPrice: 56,
+                    thumbnail: "https://cdn.dummyjson.com/product-images/88/thumbnail.jpg"
+                },
+                {
+                    _id: uuidv4(),
+                    title: "Oil Free Moisturizer 100ml",
+                    price: 40,
+                    img:'https://assets.beautyhub.co.ke/wp-content/uploads/2019/01/30121703/cosrx_oil_free_ultra_moisturizing_lotion.jpg',
+                   quantity: 2,
+                    total: 80,
+                    discountPercentage: 13.1,
+                            thumbnail: "https://cdn.dummyjson.com/product-images/54/thumbnail.jpg"
+                }
+            ,
+                {
+                    _id: uuidv4(),
+                    title: "ank Tops for Womens/Girls",
+                    price: 50,
+                    img:'https://oldnavy.gap.com/webcontent/0053/277/066/cn53277066.jpg',
+                   quantity: 2,
+                    total: 100,
+                    discountPercentage: 12.05,
+                    discountedPrice: 88,
+                    thumbnail: "https://cdn.dummyjson.com/product-images/37/thumbnail.jpg"
+                },
+                {
+                    _id: uuidv4(),
+                    title: "Chain Pin Tassel Earrings",
+                    price: 45,
+                    img:'https://i.pinimg.com/originals/b5/14/15/b51415b0b56b1e4ae231bfb94012781b.jpg',
+                   quantity: 3,
+                    total: 135,
+                    discountPercentage: 17.75,
+                    discountedPrice: 111,
+                    thumbnail: "https://cdn.dummyjson.com/product-images/80/thumbnail.jpg"
+                },
+                {
+                    _id: uuidv4(),
+                    title: "Stylish Luxury Digital Watch",
+                    price: 57,
+                    img:'https://i.pinimg.com/originals/6b/1b/de/6b1bdefb293721fcd38543e19962f3ed.jpg',
+                   quantity: 3,
+                    total: 171,
+                    discountPercentage: 9.03,
+                    discountedPrice: 156,
+                    thumbnail: "https://cdn.dummyjson.com/product-images/68/thumbnail.webp"
+                },
+                {
+                    _id: uuidv4(),
+                    title: "Round Silver Frame Sun Glasses",
+                    price: 19,
+                    img:'https://www.steampunkgoggles.com/wp-content/uploads/bulky-bridge-silver-mirror-34.jpg',
+                   quantity: 1,
+                    total: 19,
+                    discountPercentage: 10.1,
+                    discountedPrice: 17,
+                    thumbnail: "https://cdn.dummyjson.com/product-images/81/thumbnail.jpg"
+                },
+                {
+                    _id: uuidv4(),
+                    title: "Cycle Bike Glow",
+                    price: 35,
+                    img:'https://www.jeweliadesigns.com/cdn/shop/products/IMG_6726.JPG?v=1464233766',
+                   quantity: 1,
+                    total: 35,
+                    discountPercentage: 11.08,
+                    discountedPrice: 31,
+                    thumbnail: "https://cdn.dummyjson.com/product-images/90/thumbnail.jpg"
+                }
+           ,
+                {
+                    _id: uuidv4(),
+                    title: "Sleeve Shirt Womens",
+                    price: 90,
+                    img:'https://www.bigw.com.au/medias/sys_master/images/images/h24/h80/45149033267230.jpg',
+                   quantity: 1,
+                    total: 90,
+                    discountPercentage: 10.89,
+                    discountedPrice: 80,
+                    thumbnail: "https://cdn.dummyjson.com/product-images/36/thumbnail.jpg"
+                },
+                {
+                    _id: uuidv4(),
+                    title: "Pubg Printed Graphic T-Shirt",
+                    price: 46,
+                    img:'https://tse1.mm.bing.net/th/id/OIP.UgoqHtLNcm9fgG8BwA9ORQHaHa?pid=Api&P=0&h=180',
+                   quantity: 1,
+                    total: 46,
+                    discountPercentage: 16.44,
+                    discountedPrice: 38,
+                    thumbnail: "https://cdn.dummyjson.com/product-images/54/thumbnail.jpg"
+                },
+                {
+                    _id: uuidv4(),
+                    title: "perfume Oil",
+                    price: 13,
+                    img:'https://cdn.shopify.com/s/files/1/1209/3908/products/perfume_square_2.jpg?v=1601390301',
+                   quantity: 3,
+                    total: 39,
+                    discountPercentage: 8.4,
+                    discountedPrice: 36,
+                    thumbnail: "https://cdn.dummyjson.com/product-images/11/thumbnail.jpg"
+                },
+                {
+                    _id: uuidv4(),
+                    title: "Sneaker shoes",
+                    price: 120,
+                    img:'https://img.freepik.com/premium-photo/sneaker-shoe_113255-36955.jpg',
+                   quantity: 2,
+                    total: 240,
+                    discountPercentage: 10.37,
+                    discountedPrice: 215,
+                    thumbnail: "https://cdn.dummyjson.com/product-images/47/thumbnail.jpeg"
+                },
+                {
+                    _id: uuidv4(),
+                    title: "Leather Strap Skeleton Watch",
+                    price: 46,
+                    img:'https://www.pngmart.com/files/6/Watch-PNG-Background-Image.png',
+                   quantity: 3,
+                    total: 138,
+                    discountPercentage: 10.2,
+                    discountedPrice: 124,
+                    thumbnail: "https://cdn.dummyjson.com/product-images/64/thumbnail.jpg"
+                }
+        ,
+                {
+                    _id: uuidv4(),
+                    title: "Wholesale cargo lashing Belt",
+                    price: 930,
+                    img:'https://static.wixstatic.com/media/1c0c91_f973f823376d49bf80a2b8e9bf6b4035~mv2.jpg/v1/fill/w_800,h_800,al_c/1c0c91_f973f823376d49bf80a2b8e9bf6b4035~mv2.jpg',
+                   quantity: 1,
+                    total: 930,
+                    discountPercentage: 17.67,
+                    discountedPrice: 766,
+                    thumbnail: "https://cdn.dummyjson.com/product-images/95/thumbnail.jpg"
+                },
+                {
+                    _id: uuidv4(),
+                    title: "Women Sweaters Wool",
+                    price: 600,
+                    img:'https://tse1.mm.bing.net/th/id/OIP.swGGEUDylG6dVX6wwbyaNAHaG_?pid=Api&P=0&h=180',
+                   quantity: 2,
+                    total: 1200,
+                    discountPercentage: 17.2,
+                    discountedPrice: 994,
+                    thumbnail: "https://cdn.dummyjson.com/product-images/39/thumbnail.jpg"
+                },
+                  {
+                    _id: uuidv4(),
+                    title: "lighting ceiling kitchen",
+                    price: 30,
+                    img:'https://cdn.mos.cms.futurecdn.net/HzbfzCMMDCkFaef4jUiN3b-1200-80.jpg',
+                   quantity: 2,
+                    total: 60,
+                    discountPercentage: 14.89,
+                    discountedPrice: 51,
+                    thumbnail: "https://cdn.dummyjson.com/product-images/96/thumbnail.jpg"
+                },
+                {
+                    _id: uuidv4(),
+                    title: "Black Motorbike",
+                    price: 569,
+                    img:'https://cdn.suwalls.com/wallpapers/motorcycles/black-kawasaki-ninja-zx-6r-52841-1920x1200.jpg',
+                   quantity: 3,
+                    total: 1707,
+                    discountPercentage: 13.63,
+                    discountedPrice: 1474,
+                    thumbnail: "https://cdn.dummyjson.com/product-images/91/thumbnail.jpg"
+                },
+                {
+                    _id: uuidv4(),
+                    title: "Infinix INBOOK",
+                    price: 1099,
+                    img:'https://www.gizmochina.com/wp-content/uploads/2021/12/Infinix-InBook-X1-Starfull-Grey-1024x599.jpg',
+                   quantity: 1,
+                    total: 1099,
+                    discountPercentage: 11.83,
+                    discountedPrice: 969,
+                    thumbnail: "https://cdn.dummyjson.com/product-images/9/thumbnail.jpg"
+                },
+                {
+                    _id: uuidv4(),
+                    title: "Hyaluronic Acid Serum",
+                    price: 19,
+                    img:'https://media6.ppl-media.com/tr:h-750,w-750,c-at_max,dpr-2/static/img/product/332924/l-oreal-paris-revitalift-1-5-percentage-hyaluronic-acid-serum-15-ml-59_6_display_1685626486_2a4ad2de.jpg',
+                   quantity: 1,
+                    total: 19,
+                    discountPercentage: 13.31,
+                    discountedPrice: 16,
+                    thumbnail: "https://cdn.dummyjson.com/product-images/16/thumbnail.jpg"
+                },
+                {
+                    _id: uuidv4(),
+                    title: "Pubg Printed Graphic T-Shirt",
+                    price: 46,
+                    img:'https://tse3.mm.bing.net/th/id/OIP.cZxGJqMlM_NLvGehVZceSwHaHa?pid=Api&P=0&h=180',
+                   quantity: 3,
+                    total: 138,
+                    discountPercentage: 16.44,
+                    discountedPrice: 115,
+      
+        },
+       
+                {
+                    _id: uuidv4(),
+                    title: "Orange Essence Food Flavou",
+                    price: 14,
+                    img:'https://img2.exportersindia.com/product_images/bc-full/2023/6/8288854/orange-flavour-essence-1686218262-6928974.png',
+                   quantity: 3,
+                    total: 42,
+                    discountPercentage: 8.04,
+                    discountedPrice: 39,
+                    thumbnail: "https://cdn.dummyjson.com/product-images/23/thumbnail.jpg"
+                },
+                {
+                    _id: uuidv4(),
+                    title: "Black Motorbike",
+                    price: 569,
+                    img:'https://cdn.suwalls.com/wallpapers/motorcycles/black-kawasaki-ninja-zx-6r-52841-1920x1200.jpg',
+                   quantity: 1,
+                    total: 569,
+                    discountPercentage: 13.63,
+                    discountedPrice: 491,
+                    thumbnail: "https://cdn.dummyjson.com/product-images/91/thumbnail.jpg"
+                },
+                {
+                    _id: uuidv4(),
+                    title: "Malai Maxi Dress",
+                    price: 50,
+                    img:'https://img.perniaspopupshop.com/catalog/product/m/r/MRVZ022443_1.JPG?impolicy=detailimageprod',
+                   quantity: 2,
+                    total: 100,
+                    discountPercentage: 5.07,
+                    discountedPrice: 95,
+                    thumbnail: "https://cdn.dummyjson.com/product-images/45/thumbnail.jpg"
+                },
+                {
+                    _id: uuidv4(),
+                    title: "Square Sunglasses",
+                    price: 28,
+                    img:'https://m.macbella.com/wp-content/uploads/2020/07/04065854/FF0411SD.jpg',
+                   quantity: 1,
+                    total: 28,
+                    discountPercentage: 13.89,
+                    discountedPrice: 24,
+                    thumbnail: "https://cdn.dummyjson.com/product-images/84/thumbnail.jpg"
+                },
+                {
+                    _id: uuidv4(),
+                    title: "Stainless Steel Women",
+                    price: 35,
+                    img:'https://tse4.mm.bing.net/th/id/OIP.ckFwev4mhz483gppZPxYHwHaHa?pid=Api&P=0&h=180',
+                   quantity: 3,
+                    total: 105,
+                    discountPercentage: 8.98,
+                    discountedPrice: 96,
+                    thumbnail: "https://cdn.dummyjson.com/product-images/70/thumbnail.jpg"
+                }
+         
+ ]
